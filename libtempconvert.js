112da484959f59cc7c691326d3a69d46be9c3f6a
Marvin <24806960+MagicMarvMan@users.noreply.github.com>
2019-06-13 20:42:25 +0200

    Create libtempconvert.js

---
+++ libtempconvert.js
@@ -0,0 +1,23 @@
+function celsiusToFahrenheit(celsius) {
+  return ((celsius * 1.8) + 32)
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+  return ((fahrenheit - 32) / 1.8)
+}
+
+function kelvinToCelsius(kelvin) {
+  return (kelvin - 273.15)
+}
+
+function celsiusToKelvin(celsius) {
+  return (celsius + 273.15)
+}
+
+function kelvinToFahrenheit(kelvin) {
+  return (((kelvin - 273.15) * 1.8) + 32)
+}
+
+function fahrenheitToKelvin(fahrenheit) {
+  return (((fahrenheit - 32) / 1.8) + 273.15)
+}
